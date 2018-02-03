4e1251657e12f9729973280c753907b8673e7512
JixinZhang <zzjixin@gmail.com>
2018-02-03 14:02:27 +0800

    added LabelAlignment

--- ZScrollLabelDemo/ZScrollLabelDemo/ViewController.m
+++ ZScrollLabelDemo/ZScrollLabelDemo/ViewController.m
@@ -19,16 +19,19 @@
 
 - (ZScrollLabel *)titleLabel {
     if (!_titleLabel) {
-        _titleLabel = [[ZScrollLabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 100) / 2.0, 28, 100, 20)];
+        _titleLabel = [[ZScrollLabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 100) / 2.0, 60, 100, 20)];
         _titleLabel.textColor = [UIColor blackColor];
         _titleLabel.scrollDuration = 13;
+        _titleLabel.layer.borderWidth = 0.5;
+        _titleLabel.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        _titleLabel.layer.cornerRadius = 3.0;
     }
     return _titleLabel;
 }
 
 - (ZScrollLabel *)contentLabel {
     if (!_contentLabel) {
-        _contentLabel = [[ZScrollLabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 120) / 2.0, 80, 120, 20)];
+        _contentLabel = [[ZScrollLabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 120) / 2.0, 110, 120, 20)];
         _contentLabel.textColor = [UIColor redColor];
         _contentLabel.font = [UIFont systemFontOfSize:18.0f];
         _contentLabel.scrollDuration = 13;
@@ -41,7 +44,7 @@
 
 - (ZScrollLabel *)shortLabel {
     if (!_shortLabel) {
-        _shortLabel = [[ZScrollLabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 120) / 2.0, 120, 120, 20)];
+        _shortLabel = [[ZScrollLabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 120) / 2.0, 150, 120, 20)];
         _shortLabel.textColor = [UIColor blackColor];
         _shortLabel.scrollDuration = 13;
         _shortLabel.layer.borderWidth = 0.5;
@@ -55,14 +58,14 @@
     [super viewDidLoad];
     self.automaticallyAdjustsScrollViewInsets = NO;
     
-    self.titleLabel.text = @"跑马灯label";
+    self.titleLabel.text = @"居左显示";
     self.titleLabel.labelAlignment = ZScrollLabelAlignmentLeft;
     [self.view addSubview:self.titleLabel];
     
     self.contentLabel.text = @"一身诗意千寻瀑，万古人间四月天";
     [self.view addSubview:self.contentLabel];
     
-    self.shortLabel.text = @"Hello, world";
+    self.shortLabel.text = @"居右显示";
     self.shortLabel.labelAlignment = ZScrollLabelAlignmentRight;
     [self.view addSubview:self.shortLabel];
 }
